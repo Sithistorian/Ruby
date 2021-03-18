52b04a6a08187396aebc35d17a112de983b633a6
Tristan <tristanshawnjohnson@gmail.com>
2021-03-18 15:26:57 -0700

    I've created a function to get all the reviews

--- Reviews/server/index.js
+++ Reviews/server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 const SDCmongoDB = require('./mongoDB.js');
-const SDCmysqlDB = require('./mysqlDB.js');
+const db = require('./mysqlDB.js');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- Reviews/server/mysqlDB.js
+++ Reviews/server/mysqlDB.js
@@ -9,4 +9,35 @@ let connection = mysql.createConnection({
 
 connection.connect()
 
-module.exports.connection = connection;
+const getReviews = function (product_id, callback) {
+
+  //I want to get all the reviews with the photos but non of the reviews that have been reported i.e. have a 1 for their reported value
+
+  let queryString = `SELECT * FROM Reviews WHERE product_id = ?`
+
+  connection.query(queryString, [product_id], (err, data) => {
+    if (err) {
+      callback(err);
+      console.error(err, null);
+    } else {
+      callback(null, data);
+    }
+
+  })
+
+}
+
+console.log(getReviews(
+  1, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+    return data;
+  }
+}));
+
+
+module.exports.connection = {
+  getReviews
+};
